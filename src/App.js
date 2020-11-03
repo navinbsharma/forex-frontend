308b13613debb34417c3a08b12f41f60c4bca7a7
Navin Sharma <navin@litmusblox.io>
2020-11-03 19:55:05 +0530

    add converter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import './componenets/navbar'
+import Navbar from './componenets/navbar';
+import Converter from './componenets/Converter';
+import CompanySearchData from "./componenets/CompanySearchData";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>Hello</div>
+      <div className="header p-4 mr-auto">
+        <Navbar />
+        <Converter />
+      </div>
+      <div>
+        <CompanySearchData />
+      </div>
     </div>
   );
 }
---
+++ src/componenets/navbar.js
@@ -0,0 +1,32 @@
+import logo from "../FX.png";
+import { Navbar, Form, Button, FormControl, Nav, NavDropdown, OverlayTrigger, Tooltip } from 'react-bootstrap'
+
+function NavBar() {
+
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            Feature Comming Soon !!!
+        </Tooltip>
+    );
+
+    return (<div>
+        <Navbar bg="transparent" expand="lg">
+            <Navbar.Brand className="title nav-item-color" href="#home">FOREX AGGREGATOR</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <OverlayTrigger placement="bottom" delay={{ show: 250, hide: 300 }} overlay={renderTooltip}>
+                        <Nav.Link href="#link"><span className="nav-item-color">Near By Agent</span></Nav.Link>
+                    </OverlayTrigger>
+                    <OverlayTrigger placement="bottom" delay={{ show: 250, hide: 300 }} overlay={renderTooltip}>
+                        <Nav.Link href="#link"><span className="nav-item-color">Near By Bank</span></Nav.Link>
+                    </OverlayTrigger>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </div>
+    )
+};
+
+export default NavBar;
+
